caaab6f297e1556880e40e6009926e9abfc6f334
IntelliFinder <snirhordan@gmail.com>
2017-01-24 20:34:43 +0200

    Tested
    Debugged with tuned css

--- memoryGameHordanSenir/src/js/memoryGame.js
+++ memoryGameHordanSenir/src/js/memoryGame.js
@@ -1,36 +1,44 @@
+"use strict";
 
+function Blog(props) {
 
-//render in <ul>
-//add tansitions
-//randomize
-
-function Pic() {
-const Picture=props.img.map((prop) =>  
-    <div className="Pic" key={prop.id}>
-        <img className="memory" //imortant for css when hovering and transitioning when someone presses the right button
-             src={prop.avatarUrl}
-             alt={prop.name} 
-             height="150"
-             width="150"/>
-    </div>
+  var content = props.posts.map(function (post) { //randomize find random map function in js
+    return React.createElement(
+      "div",
+      { key: post.id, className: "container-fluid" },
+      React.createElement("img", { className: "img-responsive center-block img-thumbnail",
+        src: post.content,
+        alt: post.title,
+        height: "150",
+        width: "150" })
+    );
+  });
+  return React.createElement(
+    "div",
+    null,
+    React.createElement(
+      "h1",
+      { className: 'page-header' },
+      'Memory Game'
+    ),
+    React.createElement(
+      "section",
+      null,
+      content
+    ),
+    React.createElement("hr", null),
+    React.createElement(
+      "section",
+      null,
+      content
+    ), //attach button to randomize
+  React.createElement(
+      "footer",
+      null ,
+      '©all Rights Reserved to Hordan Senir 2017'
+    )
   );
-return (
-    <div>
-       <Section>
-      {Picture}
-      </Section>
-      <hr />
-      <Section>
-      {Picture}
-      </Section>
-      </div>);
 }
-const img = [ { id: 1,
-    name: "Little Cat",
-    avatarUrl: 'http://placekitten.com/g/64/64'}]
- 
-};
-ReactDOM.render(
-  <Pic img={img} />,
-  document.getElementById('root')
-);
+
+var posts = [{ id: 1, title: 'Hello World', content: './src/image/pic.jpg' }, { id: 2, title: 'Installation', content: './src/image/pic2.jpg' }, { id: 3, title: 'Hello World', content: './src/image/pic3.jpg' }, { id: 4, title: 'Installation', content: './src/image/pic4.jpg' }, { id: 5, title: 'Hello World', content: './src/image/pic5.jpg' }, { id: 6, title: 'Installation', content: './src/image/pic6.jpg' }, { id: 7, title: 'Installation', content: './src/image/pic7.jpg' }, { id: 8, title: 'Installation', content: './src/image/pic8.jpg' }];
+ReactDOM.render(React.createElement(Blog, { posts: posts }), document.getElementById('root'));
