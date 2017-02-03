7131a3529fe09da251514ef60a509e7b737156ee
IntelliFinder <snirhordan@gmail.com>
2017-02-03 10:57:00 +0200

    Create DEV.jsx
    some approaches for functionality

---
+++ memoryGameHordanSenir/src/js/DEV.jsx
@@ -0,0 +1,72 @@
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <img  className= {this.state.isToggleOn ? 'ON' : 'OFF',
+       src= "http://www.w3schools.com/images/w3schools_green.jpg",
+        alt= "whatever",
+        height="150",
+        width="150" /img>
+      <button id='this' onClick={this.handleClick} className= {this.state.isToggleOn ? 'ON' : 'OFF'}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);
+
+
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {value: ''};
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value});
+  }
+  
+  render() {
+    const value = this.state.value;
+    return (
+      <fieldset>
+        <legend>Enter temperature in Celsius:</legend>
+        <input
+          value={value}
+          onChange={this.handleChange} />
+        <BoilingVerdict
+          celsius={parseFloat(value)} />
+      </fieldset>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Calculator />,
+  document.getElementById('root')
+);
